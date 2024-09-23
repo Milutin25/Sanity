5e85b669a714ae1a22b5a1a93aa2948384a5c62b
Natasa90 <natasa.golubovic90@gmail.com>
2024-09-23 16:25:36 +0200

    initial commit

---
+++ blogs/schemaTypes/blogType.ts
@@ -0,0 +1,39 @@
+import {defineField, defineType} from 'sanity'
+
+export const blogType = defineType({
+    name: 'blog',
+    title: 'Blog', 
+    type: 'document',
+    fields: [
+        defineField({
+            name: 'name',
+            type: 'string'
+        }),
+        defineField({
+            name: 'id',
+            type: 'number'
+        }),
+        defineField({
+            name: 'image',
+            type: 'image'
+        }),
+        defineField({
+            name: 'slug',
+            type: 'slug',
+            options: {source: 'name'},
+            validation: (rule) => rule
+            .required()
+            .error(`Required to generate a page on the website`),
+            hidden: ({document}) => !document?.name
+        }), 
+        defineField({
+            name: 'date',
+            type: 'datetime'
+        }),
+        defineField({
+            name: 'text',
+            type: 'array',
+            of: [{type: 'block'}]
+        })
+    ]
+})
---
+++ blogs/schemaTypes/index.ts
@@ -0,0 +1,5 @@
+import { blogType } from "./blogType"
+
+export const schemaTypes = [
+    blogType
+]
